e46eb75d81b2f51814079daeef1c851667534d11
Kian K <epicgreenturtles@gmail.com>
2025-07-22 12:15:15 -0600

    made .py file number 8 have less ines of code

--- python-hello/8-concat_edges.py
+++ python-hello/8-concat_edges.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-# Define the input string
-str1 = "python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-words = str1.split()
-if len(words) > 6:
-    target = words[5] + " " + words[6]
-    print(target)
+str = """Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"""
+str = str[39:66]
+str = str + " with python"
+print(str)
